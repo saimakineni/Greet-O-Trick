5b1659d14e69183271537d3d044efbf0fb829301
Sainadh Makineni <msainadh@gmail.com>
2021-01-03 01:23:52 -0500

    restructured folders and created utils

---
+++ src/utils/utils.py
@@ -0,0 +1,8 @@
+def call_google_api():
+
+def generate_greeting(name,relation,context,image):
+    # get keywords using google api
+    # Match google API keywords and input text to similarity matching algorithms
+    
+
+
